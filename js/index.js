2b96633e3495b8863698931be665a55e09be0cd6
qiaoyukeji <qiaoyukeji@gmail.com>
2020-05-26 21:39:22 +0800

    饼图2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  * @Author: qiaoyurensheng@163.com
  * @Date: 2020-05-26 12:01:41
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-26 18:07:01
+ * @LastEditTime: 2020-05-26 21:34:32
  */
 // 通过立即执行函数，防止命名冲突与变量污染
 // 柱图1
@@ -307,3 +307,236 @@
     })
 
 })();
+
+// 折线图2
+(function () {
+    var myChart = echarts.init(document.querySelector(".line2 .chart"));
+    var option = {
+
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            top: "0%",
+            textStyle: {
+                color: "rgba(255,255,255,.5)",
+                fontSize: "12"
+            }
+        },
+
+        grid: {
+            left: "10",
+            top: "30",
+            right: "10",
+            bottom: "10",
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                boundaryGap: false,
+                data: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "26", "28", "29", "30"],
+                axisLabel: {
+                    textStyle: {
+                        color: "rgba(255,255,255,.6)",
+                        fontSize: 12
+                    }
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: "rgba(255,255,255,.2)"
+                    }
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisTick: {
+                    show: false
+                }, axisLine: {
+                    lineStyle: {
+                        color: "rgba(255,255,255,.1)"
+                    }
+                },
+                axisLabel: {
+                    textStyle: {
+                        color: "rgba(255,255,255,.6)",
+                        fontSize: 12
+                    }
+                },
+                splitLine: {
+                    lineStyle: {
+                        color: "rgba(255,255,255,.1)"
+                    }
+                }
+            }
+        ],
+        series: [
+            {
+                name: '邮件营销',
+                type: "line",
+                //第一条 线是圆滑
+                smooth: true,
+                // 单独修改线的样式
+                lineStyle: {
+                    color: "#0184d5",
+                    width: 2
+                },
+                // 填充区域
+                areaStyle: {
+                    // 渐变色，只需要复制即可
+                    color: new echarts.graphic.LinearGradient(
+                        0,
+                        0,
+                        0,
+                        1,
+                        [
+                            {
+                                offset: 0,
+                                color: "rgba(1, 132, 213, 0.4)"   // 渐变色的起始颜色
+                            },
+                            {
+                                offset: 0.8,
+                                color: "rgba(1, 132, 213, 0.1)"   // 渐变线的结束颜色
+                            }
+                        ],
+                        false
+                    ),
+                    shadowColor: "rgba(0, 0, 0, 0.1)"
+                },
+                // 设置拐点 小圆点
+                symbol: "circle",
+                // 拐点大小
+                symbolSize: 8,
+                // 设置拐点颜色以及边框
+                itemStyle: {
+                    color: "#0184d5",
+                    borderColor: "rgba(221, 220, 107, .1)",
+                    borderWidth: 12
+                },
+                // 开始不显示拐点， 鼠标经过显示
+                showSymbol: false,
+                data: [30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40],
+            },
+            {
+                name: "转发量",
+                type: "line",
+                smooth: true,
+                lineStyle: {
+                    normal: {
+                        color: "#00d887",
+                        width: 2
+                    }
+                },
+                areaStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(
+                            0,
+                            0,
+                            0,
+                            1,
+                            [
+                                {
+                                    offset: 0,
+                                    color: "rgba(0, 216, 135, 0.4)"
+                                },
+                                {
+                                    offset: 0.8,
+                                    color: "rgba(0, 216, 135, 0.1)"
+                                }
+                            ],
+                            false
+                        ),
+                        shadowColor: "rgba(0, 0, 0, 0.1)"
+                    }
+                },
+                // 设置拐点 小圆点
+                symbol: "circle",
+                // 拐点大小
+                symbolSize: 5,
+                // 设置拐点颜色以及边框
+                itemStyle: {
+                    color: "#00d887",
+                    borderColor: "rgba(221, 220, 107, .1)",
+                    borderWidth: 12
+                },
+                // 开始不显示拐点， 鼠标经过显示
+                showSymbol: false,
+                data: [130, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130, 20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20],
+            }
+        ]
+    };
+
+
+    myChart.setOption(option);
+    // 让图表跟随屏幕自适应
+    window.addEventListener("resize", function () {
+        myChart.resize();
+    });
+
+})();
+
+// 饼图1
+(function () {
+    var myChart = echarts.init(document.querySelector(".pie .chart"));
+    var option = {
+        color: [
+            "#065aab",
+            "#066eab",
+            "#0682ab",
+            "#0696ab",
+            "#06a0ab",
+        ],
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+
+        legend: {
+            // orient: 'vertical',
+            bottom: 0,
+            itemWidth: 15,
+            itemHeigth: 10,
+
+            // 修改图例组件的文字为 12px
+            textStyle: {
+                color: "rgba(255,255,255,.5)",
+                fontSize: "12"
+            }
+        },
+        series: [
+            {
+                name: '年龄分布',
+                type: 'pie',
+                radius: ['40%', '60%'],
+                center: ["50%", "40%"],
+                avoidLabelOverlap: false,
+                // 图形上的文字
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                // 链接文字与图形的线
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    { value: 1, name: "0岁以下" },
+                    { value: 4, name: "20-29岁" },
+                    { value: 2, name: "30-39岁" },
+                    { value: 2, name: "40-49岁" },
+                    { value: 1, name: "50岁以上" }
+                ],
+            }
+        ]
+    };
+
+    myChart.setOption(option)
+    // 让图表跟随屏幕自适应
+    window.addEventListener("resize", function () {
+        myChart.resize();
+    });
+})();
+
+
