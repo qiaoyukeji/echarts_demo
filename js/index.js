a86e56e3d5a0cb4336dba3fb5d0effc810f9a846
qiaoyukeji <qiaoyukeji@gmail.com>
2020-05-26 18:27:12 +0800

    折线1

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
  * @Author: qiaoyurensheng@163.com
  * @Date: 2020-05-26 12:01:41
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-26 15:33:25
+ * @LastEditTime: 2020-05-26 18:07:01
  */
 // 通过立即执行函数，防止命名冲突与变量污染
 // 柱图1
@@ -199,3 +199,111 @@
 
 })();
 
+// 折线图1
+(function () {
+    var myChart = echarts.init(document.querySelector(".line .chart"));
+    var yearData = [
+        {
+            year: '2020',  // 年份
+            data: [  // 两个数组是因为有两条线
+                [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+                [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+            ]
+        },
+        {
+            year: '2021',  // 年份
+            data: [  // 两个数组是因为有两条线
+                [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
+                [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34]
+            ]
+        }
+    ];
+    var option = {
+        // 通过color数组修改颜色
+        color: ['#00f2f1', '#ed3f35'],
+        tooltip: {
+            trigger: 'axis'
+        },
+        // 修改图例组件
+        legend: {
+            textStyle: {
+                color: "#4c9bfd"
+            },
+            right: "10%"
+        },
+        grid: {
+            top: '20%',
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            show: true,// 显示边框
+            borderColor: '#012f4a',// 边框颜色
+            containLabel: true // 包含刻度文字在内
+        },
+        xAxis: {
+            type: 'category',
+            axisTick: {
+                show: false
+            },
+            axisLabel: {
+                color: "#4c9bfd"
+            },
+            axisLine: {
+                show: false
+            },
+            boundaryGap: false,
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+        },
+        yAxis: {
+            axisTick: {
+                show: false  // 去除刻度
+            },
+            axisLabel: {
+                color: '#4c9bfd' // 文字颜色
+            },
+            // 修改分割线
+            splitLine: {
+                lineStyle: {
+                    color: '#012f4a' // 分割线颜色
+                }
+            },
+            type: 'value'
+        },
+        series: [
+            {
+                name: '新增粉丝',
+                data: yearData[0].data[0],
+                type: 'line',
+                // 折线是否圆滑
+                smooth: true,
+
+            },
+            {
+                name: '新增游客',
+                data: yearData[0].data[1],
+                type: 'line',
+                smooth: true,
+
+            }
+        ]
+    };
+
+    myChart.setOption(option)
+    // 让图表跟随屏幕自适应
+    window.addEventListener("resize", function () {
+        myChart.resize();
+    });
+    // 点击切换2020与2021
+    $(".line h2").on("click", "a", function () {
+        // alert(1)
+        // console.log($(this).index());
+        // 根据a 索引号,找到相关yearData
+        // console.log(yearData[$(this).index()]);
+        option.series[0].data = yearData[$(this).index()].data[0]
+        option.series[1].data = yearData[$(this).index()].data[1]
+        // 重新渲染图表
+        myChart.setOption(option)
+
+    })
+
+})();
